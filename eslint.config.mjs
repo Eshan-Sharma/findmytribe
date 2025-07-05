86e77976bf27405a29245a81a88a31a987033a57
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2025-07-05 16:41:40 +0530

    fix linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,23 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      // Downgrade the error to a warning
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          args: "after-used",
+          ignoreRestSiblings: true,
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
